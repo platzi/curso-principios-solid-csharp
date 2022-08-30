d2a8a54ab6a97917609cc685518e1ba56300fac6
Miguel Teheran <miguelteheran@gmail.com>
2022-08-29 21:58:58 -0500

    solucionando error del controlador

--- 5-DependencyInversion/Api.Tests/StudentTest.cs
+++ 5-DependencyInversion/Api.Tests/StudentTest.cs
@@ -8,31 +8,9 @@ namespace Api.Tests;
 
 public class StudentTest 
 {
-    [Fact]
-    public void GetStudent()
-    {
-        var studentController = new StudentController();
-
-        var resultGetStudents = studentController.Get();
-
-        Assert.NotNull(resultGetStudents);
-        Assert.Equal(3, resultGetStudents.Count());
-    }
-
-
     // [Fact]
     // public void GetStudent()
     // {
-    //     var LogbookMock = new Mock<ILogbook>();
-    //     var stundentRepositoryMock = new Mock<IStudentRepository>();
-    //     stundentRepositoryMock.Setup(p=> p.GetAll())
-    //                                     .Returns(new List<Student>()
-    //                                     {
-    //                                         new Student(1, "Pepito Pérez", new List<double>() { 3, 4.5 }),
-    //                                         new Student(2, "Mariana Lopera", new List<double>() { 4, 5 }),
-    //                                         new Student(3, "José Molina", new List<double>() { 2, 3 })
-    //                                     });
-
     //     var studentController = new StudentController();
 
     //     var resultGetStudents = studentController.Get();
@@ -40,4 +18,26 @@ public class StudentTest
     //     Assert.NotNull(resultGetStudents);
     //     Assert.Equal(3, resultGetStudents.Count());
     // }
+
+
+    [Fact]
+    public void GetStudent()
+    {
+        var LogbookMock = new Mock<ILogbook>();
+        var stundentRepositoryMock = new Mock<IStudentRepository>();
+        stundentRepositoryMock.Setup(p=> p.GetAll())
+                                        .Returns(new List<Student>()
+                                        {
+                                            new Student(1, "Pepito Pérez", new List<double>() { 3, 4.5 }),
+                                            new Student(2, "Mariana Lopera", new List<double>() { 4, 5 }),
+                                            new Student(3, "José Molina", new List<double>() { 2, 3 })
+                                        });
+
+        var studentController = new StudentController(stundentRepositoryMock.Object, LogbookMock.Object);
+
+        var resultGetStudents = studentController.Get();
+
+        Assert.NotNull(resultGetStudents);
+        Assert.Equal(3, resultGetStudents.Count());
+    }
 }
